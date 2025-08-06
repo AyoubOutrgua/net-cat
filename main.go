2aed054b5b0c9019c4c638a77e559d561cfa2095
Yayouboutrgua <outrgua1949@gmail.com>
2025-08-06 17:36:27 +0100

    update

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"net"
 	"os"
+	"strings"
+	"sync"
 	"time"
 )
 
@@ -27,6 +29,10 @@ func main() {
 	defer listener.Close()
 
 	for {
+		if len(clients) > 2 {
+			fmt.Println("Connections ghir 10 baraka")
+			continue
+		}
 		conn, err := listener.Accept()
 		if err != nil {
 			fmt.Println("Error :", err)
@@ -36,37 +42,63 @@ func main() {
 	}
 }
 
-var welcomeMsg string = "Welcome to TCP-Chat!\n         _nnnn_\n        dGGGGMMb\n       @p~qp~~qMb\n       M|@||@) M|\n       @,----.JM|\n      JS^\\__/  qKL\n     dZP        qKRb\n    dZP          qKKb\n   fZP            SMMb\n   HZM            MMMM\n   FqM            MMMM\n __| \".        |\\dS\"qML\n |    `.       | `' \\Zq\n_)      \\.___.,|     .'\n\\____   )MMMMMP|   .'\n     `-'       `--'\n[ENTER YOUR NAME]:"
+var (
+	welcomeMsg   string = "Welcome to TCP-Chat!\n         _nnnn_\n        dGGGGMMb\n       @p~qp~~qMb\n       M|@||@) M|\n       @,----.JM|\n      JS^\\__/  qKL\n     dZP        qKRb\n    dZP          qKKb\n   fZP            SMMb\n   HZM            MMMM\n   FqM            MMMM\n __| \".        |\\dS\"qML\n |    `.       | `' \\Zq\n_)      \\.___.,|     .'\n\\____   )MMMMMP|   .'\n     `-'       `--'\n[ENTER YOUR NAME]:"
+	clients             = make(map[net.Conn]string)
+	messages            = make(map[net.Conn][]string)
+	mutex        sync.Mutex
+	countClients = 0
+)
 
 func HandleClient(conn net.Conn) {
-	fmt.Println("connect seccess")
 	defer conn.Close()
 	conn.Write([]byte(welcomeMsg))
-	fmt.Println("client jdid connecter:", conn.RemoteAddr())
-
 	reader := bufio.NewReader(conn)
-	name := ""
-	msgMap := make(map[string][]string)
+
+	username, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Name read error:", err)
+		return
+	}
+	username = strings.TrimSpace(username)
+	timeNow := time.Now().Format("2006-01-02 15:04:05")
+	SendMessage(fmt.Sprintf("\n🟢 %s has joined the chat\n", username), conn, fmt.Sprintf("[%s][%s]:", timeNow, username))
+
+	clients[conn] = username
+
+	// conn.Write([]byte("[" + timeNow + "] [" + username + "] :"))
+
 	for {
+		conn.Write([]byte(fmt.Sprintf("[%s][%s]:", timeNow, username)))
+
 		msg, err := reader.ReadString('\n')
 		if err != nil {
-			fmt.Println("client khraj:", conn.RemoteAddr())
-			return
+			fmt.Println("🔴", username, "disconnected")
+			break
 		}
-		if name == "" {
-			name = msg
+		msg = strings.TrimSpace(msg)
+		if msg == "" {
 			continue
 		}
-		if name != "" {
-		}
-		now := time.Now()
-		timeNow := now.Format("2006-01-02 15:04:05")
-		slice := []string{timeNow, name, msg}
-		fmt.Println("slice :", slice)
-		msgMap[name] = slice
-		value := msgMap[name]
-		fmt.Printf("[%s] [%s] [%s] :\n", value[1], value[1], value[2])
+		fullMsg := fmt.Sprintf("[%s][%s]: %s\n", timeNow, username, msg)
+		SendMessage(fullMsg, conn, "")
+		// for k, v := range messages {
+		// 	if k != conn {
+		// 		k.Write([]byte("[" + v[0] + "] [" + v[1] + "] :" + v[2]))
+		// 	}
+		// }
 
-		conn.Write([]byte("wsl msg ok :" + msg))
+	}
+}
+
+func SendMessage(message string, sender net.Conn, str string) {
+	for conn := range clients {
+		if conn != sender {
+			_, err := conn.Write([]byte(message))
+			_, err2 := conn.Write([]byte(str))
+			if err != nil || err2 != nil {
+				fmt.Println("Failed to send message to", clients[conn])
+			}
+		}
 	}
 }
