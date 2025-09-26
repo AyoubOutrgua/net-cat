23c75bf96453551c7b38049d4a06d8408b5cfe36
ayouboutrgua <outrgua1949@gmail.com>
2025-09-26 18:14:59 +0100

    add color

--- functions/handleClient.go
+++ functions/handleClient.go
@@ -13,7 +13,7 @@ func HandleClient(conn net.Conn) {
 	defer conn.Close()
 
 	var errWrite error
-	_, errWrite = conn.Write([]byte(welcomeMsg))
+	_, errWrite = conn.Write([]byte("\033[33m" + welcomeMsg))
 	if errWrite != nil {
 		fmt.Println(errWrite)
 		return
@@ -34,18 +34,18 @@ func HandleClient(conn net.Conn) {
 		username = strings.TrimSpace(name)
 
 		if !IsPrintableRange(username) {
-			_, errWrite = conn.Write([]byte("❌​ Invalid Username\n"))
+			_, errWrite = conn.Write([]byte("\033[31m❌​ Invalid Username\n"))
 			if errWrite != nil {
 				fmt.Println(errWrite)
 				return
 			}
-			_, errWrite = conn.Write([]byte("[ENTER YOUR NAME]: "))
+			_, errWrite = conn.Write([]byte("\033[34m[ENTER YOUR NAME]: "))
 			if errWrite != nil {
 				fmt.Println(errWrite)
 				return
 			}
 		} else if !IsValidUsername(username) {
-			_, errWrite = conn.Write([]byte("❌​ Invalid Username\n"))
+			_, errWrite = conn.Write([]byte("\033[31m❌​ Invalid Username\n"))
 			if errWrite != nil {
 				fmt.Println(errWrite)
 				return
@@ -62,7 +62,7 @@ func HandleClient(conn net.Conn) {
 			mutexClient.Unlock()
 
 			if len(clients) > 10 {
-				_, errWrite = conn.Write([]byte("The room is full"))
+				_, errWrite = conn.Write([]byte("\033[31mThe room is full"))
 				if errWrite != nil {
 					fmt.Println(errWrite)
 					return
@@ -75,10 +75,10 @@ func HandleClient(conn net.Conn) {
 			}
 
 			if !checkConnection {
-				SendMessage(fmt.Sprintf("🟢 %s has joined our chat...\n", username), conn)
+				SendMessage(fmt.Sprintf("\033[32m🟢 %s has joined our chat...\n", username), conn)
 				mutexMessage.Lock()
 				for _, msg := range messages {
-					_, errWrite = conn.Write([]byte(msg))
+					_, errWrite = conn.Write([]byte("\033[0m" + msg))
 					if errWrite != nil {
 						fmt.Println(errWrite)
 						return
@@ -92,14 +92,14 @@ func HandleClient(conn net.Conn) {
 
 	for {
 		timeNow = time.Now().Format("2006-01-02 15:04:05")
-		_, errWrite = conn.Write([]byte(fmt.Sprintf("[%s][%s]:", timeNow, username)))
+		_, errWrite = conn.Write([]byte(fmt.Sprintf("\033[0m[%s][%s]:", timeNow, username)))
 		if errWrite != nil {
 			fmt.Println(errWrite)
 			return
 		}
 		msg, err := reader.ReadString('\n')
 		if err != nil {
-			SendMessage(fmt.Sprintf("​🔴 %s has left our chat...\n", username), conn)
+			SendMessage(fmt.Sprintf("​\033[31m🔴 %s has left our chat...\n", username), conn)
 			mutexClient.Lock()
 			delete(clients, conn)
 			mutexClient.Unlock()
@@ -107,7 +107,7 @@ func HandleClient(conn net.Conn) {
 		}
 		msg = strings.TrimSpace(msg)
 		if len(msg) > 2000 {
-			_, errWrite = conn.Write([]byte("❌​ You can't write a message over 2000 letters\n"))
+			_, errWrite = conn.Write([]byte("\033[31m❌​ You can't write a message over 2000 letters\n"))
 			if errWrite != nil {
 				fmt.Println(errWrite)
 				return
@@ -116,7 +116,7 @@ func HandleClient(conn net.Conn) {
 			SendMessage("", conn)
 		} else {
 			timeNow = time.Now().Format("2006-01-02 15:04:05")
-			fullMsg := fmt.Sprintf("[%s][%s]:%s\n", timeNow, username, msg)
+			fullMsg := fmt.Sprintf("\033[0m[%s][%s]:%s\n", timeNow, username, msg)
 			SendMessage(fullMsg, conn)
 			mutexMessage.Lock()
 			messages = append(messages, fullMsg)
--- functions/sendMessage.go
+++ functions/sendMessage.go
@@ -9,16 +9,16 @@ import (
 // A function that send messages to all connections expect the sender's connection
 func SendMessage(fullMsg string, sender net.Conn) {
 	var errWrite error
-	timeNow := time.Now().Format("2006-01-02 15:04:05")
+	timeNow := time.Now().Format("\033[0m2006-01-02 15:04:05")
 	mutexClient.Lock()
 	for conn, username := range clients {
 		if conn != sender {
 			if fullMsg != "" {
-				_, errWrite = conn.Write([]byte("\n" + fullMsg))
+				_, errWrite = conn.Write([]byte("\033[0m\n" + fullMsg))
 				if errWrite != nil {
 					fmt.Println(errWrite)
 				}
-				_, errWrite = conn.Write([]byte(fmt.Sprintf("[%s][%s]:", timeNow, username)))
+				_, errWrite = conn.Write([]byte(fmt.Sprintf("\033[0m[%s][%s]:", timeNow, username)))
 				if errWrite != nil {
 					fmt.Println(errWrite)
 				}
